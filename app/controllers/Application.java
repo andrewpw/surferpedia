7e1aa756ed5191d0eaf092d2e9202e9159c3410d
Andrew Pearson-Wood <andrewpw133@gmail.com>
2013-11-13 10:48:34 -1000

    Start of WOD

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -23,7 +23,7 @@ public class Application extends Controller {
    * Returns the home page. 
    * @return The resulting home page. 
    */
-  public static Result index() { 
+  public static Result index() {  
     return ok(Index.render("Welcome to the home page."));
   }
   
