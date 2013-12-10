afaed6ffac2a8c3a4348f53951f59a870914c622
Eduard Gamiao <edgamiao@gmail.com>
2013-12-09 19:15:18 -1000

    Testing experiment.

--- test/test/IntegrationTest.java
+++ test/test/IntegrationTest.java
@@ -3,6 +3,7 @@ package test;
 import org.junit.Test;
 import play.test.TestBrowser;
 import play.libs.F.Callback;
+import test.pages.IndexPage;
 import static play.test.Helpers.HTMLUNIT;
 import static play.test.Helpers.inMemoryDatabase;
 import static play.test.Helpers.fakeApplication;
@@ -24,11 +25,9 @@ public class IntegrationTest {
   public void test() {
     running(testServer(PORT, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
       public void invoke(TestBrowser browser) {
-        browser.goTo("http://localhost:3333");
-        assertThat(browser.pageSource()).contains("home page");
-
-        browser.goTo("http://localhost:3333/page1");
-        assertThat(browser.pageSource()).contains("Page1");
+        IndexPage indexPage = new IndexPage(browser.getDriver(), PORT);
+        browser.goTo(indexPage);
+        indexPage.isAt();
       }
     });
   }
