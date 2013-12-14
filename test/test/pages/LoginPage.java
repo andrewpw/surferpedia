ecbf2c108158b6291ed1a89d4d005a1d6e3f3fac
Eduard Gamiao <edgamiao@gmail.com>
2013-12-13 18:49:23 -1000

    Testing update.
    Still not working.

--- test/test/pages/LoginPage.java
+++ test/test/pages/LoginPage.java
@@ -12,7 +12,7 @@ import static org.fest.assertions.Assertions.assertThat;
  * Illustration of the Page Object Pattern in Fluentlenium.  
  * @author Philip Johnson
  */
-public class IndexPage extends FluentPage {
+public class LoginPage extends FluentPage {
   private String url;
   
   /**
@@ -20,7 +20,7 @@ public class IndexPage extends FluentPage {
    * @param webDriver The driver.
    * @param port The port.
    */
-  public IndexPage(WebDriver webDriver, int port) {
+  public LoginPage(WebDriver webDriver, int port) {
     super(webDriver);
     this.url = "http://localhost:" + port;
   }
@@ -32,29 +32,14 @@ public class IndexPage extends FluentPage {
   
   @Override
   public void isAt() {
-    assertThat(title()).isEqualTo("Index");
+    assertThat(title()).isEqualTo("Login");
   }
   
   /**
    * Click search button.
    */
-  public void clickSearchButton() {
-    submit("#search");
-  }
-  
-  /**
-   * Click login link.
-   */
-  public void goToLogin() {
-   find("#login").click();
-  }
-  
-  /**
-   * Check if logged in.
-   * @return True if logged in, false if logged out.
-   */
-  public boolean isLoggedIn() {
-    return !find("#logout").isEmpty();
+  public void login() {
+    submit("#login");
   }
   
   /**
