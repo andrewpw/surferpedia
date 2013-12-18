97e69644800265ef41c9a51073256c2e041be125
Eduard Gamiao <edgamiao@gmail.com>
2013-12-18 10:24:26 -1000

    Added type and country to result page.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,23 +4,23 @@
 # --- !Ups
 
 create table favorite (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   surfer_id                 bigint,
   user_info_id              bigint,
   constraint pk_favorite primary key (id))
 ;
 
 create table surfer (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   name                      varchar(255),
   hometown                  varchar(255),
   awards                    varchar(255),
   carousel_url              varchar(255),
   bio_url                   varchar(255),
-  bio                       clob,
+  bio                       longtext,
   slug                      varchar(255),
   type                      varchar(255),
-  slug_defined              boolean,
+  slug_defined              tinyint(1) default 0,
   footstyle                 varchar(255),
   country                   varchar(255),
   rating                    integer,
@@ -29,7 +29,7 @@ create table surfer (
 ;
 
 create table surfer_update (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   date                      varchar(255),
   type                      varchar(255),
   target                    varchar(255),
@@ -37,21 +37,13 @@ create table surfer_update (
 ;
 
 create table user_info (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   name                      varchar(255),
   email                     varchar(255),
   password                  varchar(255),
-  admin                     boolean,
+  admin                     tinyint(1) default 0,
   constraint pk_user_info primary key (id))
 ;
-
-create sequence favorite_seq;
-
-create sequence surfer_seq;
-
-create sequence surfer_update_seq;
-
-create sequence user_info_seq;
 
 alter table favorite add constraint fk_favorite_surfer_1 foreign key (surfer_id) references surfer (id) on delete restrict on update restrict;
 create index ix_favorite_surfer_1 on favorite (surfer_id);
@@ -62,23 +54,15 @@ create index ix_favorite_userInfo_2 on favorite (user_info_id);
 
 # --- !Downs
 
-SET REFERENTIAL_INTEGRITY FALSE;
+SET FOREIGN_KEY_CHECKS=0;
 
-drop table if exists favorite;
+drop table favorite;
 
-drop table if exists surfer;
+drop table surfer;
 
-drop table if exists surfer_update;
+drop table surfer_update;
 
-drop table if exists user_info;
+drop table user_info;
 
-SET REFERENTIAL_INTEGRITY TRUE;
-
-drop sequence if exists favorite_seq;
-
-drop sequence if exists surfer_seq;
-
-drop sequence if exists surfer_update_seq;
-
-drop sequence if exists user_info_seq;
+SET FOREIGN_KEY_CHECKS=1;
 
